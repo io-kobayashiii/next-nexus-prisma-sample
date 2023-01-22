ba55560985f510cdf3a410ccfd590b8be10683bf
io-kobayashiii <io.iobayashiii@gmail.com>
2023-01-22 21:41:51 +0900

    #15: Create a resolver to create user

--- src/backend/graphql/user.ts
+++ src/backend/graphql/user.ts
@@ -1,9 +1,17 @@
-import { list, nonNull, objectType, queryField } from 'nexus';
+import {
+  list,
+  mutationType,
+  nonNull,
+  objectType,
+  queryField,
+  stringArg,
+} from 'nexus';
+import bcrypt from 'bcryptjs';
 
 export const UserType = objectType({
   name: 'UserType',
   definition(t) {
-    t.nonNull.string('id');
+    t.nonNull.int('id');
     t.nonNull.string('email');
     t.nonNull.string('name');
   },
@@ -12,12 +20,41 @@ export const UserType = objectType({
 export const UsersQueryField = queryField('users', {
   type: list(UserType),
   resolve: (_root, _args, ctx) => {
-    return [
-      {
-        id: '1',
-        email: 'user1@kobayashiii.dev',
-        name: 'user1',
+    return ctx.prisma.user.findMany();
+  },
+});
+
+export const UserMutationField = mutationType({
+  definition: (t) => {
+    t.field('createUser', {
+      type: objectType({
+        name: 'AuthPayload',
+        definition: (t) => {
+          t.nonNull.int('id');
+          t.nonNull.string('email');
+          t.nonNull.string('name');
+        },
+      }),
+      args: {
+        email: stringArg(),
+        password: stringArg(),
+        name: stringArg(),
       },
-    ];
+      resolve: async (_root, { email, password, name }, ctx) => {
+        const hashedPassword = await bcrypt.hash(password!, 10);
+        const newUser = await ctx.prisma.user.create({
+          data: {
+            email: email!,
+            password: hashedPassword,
+            name: name!,
+          },
+        });
+        return {
+          id: newUser.id,
+          email: newUser.email,
+          name: newUser.name,
+        };
+      },
+    });
   },
 });
