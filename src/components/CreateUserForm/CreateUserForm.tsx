8a9a2556ba111ad86b83b482a3ead99028301f54
io-kobayashiii <io.iobayashiii@gmail.com>
2023-01-25 00:58:41 +0900

    #20: Add validations on users page

--- src/components/CreateUserForm/CreateUserForm.tsx
+++ src/components/CreateUserForm/CreateUserForm.tsx
@@ -1,27 +1,46 @@
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useMutation } from 'urql';
 import { CreateUserDocument } from '../../generated/graphql';
+import * as yup from 'yup';
+import { SubmitHandler, useForm, FieldValues } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 type Props = {
   className?: string;
 };
 
 export const CreateUserForm = ({ className }: Props) => {
-  const [createUserResult, createUser] = useMutation(CreateUserDocument);
-  const [createUserFormValues, setCreateUserFormValues] = useState({
-    email: '',
-    password: '',
-    name: '',
+  const [_, createUser] = useMutation(CreateUserDocument);
+
+  const schema = yup.object({
+    email: yup
+      .string()
+      .email('メールアドレスの形式が正しくありません')
+      .required('この項目は必須です'),
+    password: yup.string().required('この項目は必須です'),
+    name: yup.string().required('この項目は必須です'),
   });
 
-  const onCreateButtonClick = async () => {
-    console.log('onCreateButtonClick');
-    const result = await createUser(createUserFormValues);
-    console.log(result);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = async (formInput) => {
+    console.log('onCreateButtonClick / formInput:', formInput);
+    // const result = await createUser(formInput);
+    // console.log(result);
   };
+
+  useEffect(() => {
+    console.log('errors:', errors);
+  }, [errors]);
 
   return (
     <>
@@ -31,49 +50,42 @@ export const CreateUserForm = ({ className }: Props) => {
         } max-w-500 w-100p bg-gray-800 rounded-8 p-30`}
       >
         <h2 className={'text-20 font-bold'}>Create</h2>
-        <Box component="form">
+        <Box
+          id={'create-user-form'}
+          component="form"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <TextField
-            id="email"
             label="Email"
             variant="standard"
             className={'w-100p'}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              console.log(e.currentTarget.value);
-              setCreateUserFormValues({
-                ...createUserFormValues,
-                email: e.currentTarget.value,
-              });
-            }}
+            error={'email' in errors}
+            helperText={errors.email?.message as string}
+            {...register('email')}
           />
           <TextField
-            id="password"
             label="Password"
             variant="standard"
             className={'w-100p mt-20'}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              console.log(e.currentTarget.value);
-              setCreateUserFormValues({
-                ...createUserFormValues,
-                password: e.currentTarget.value,
-              });
-            }}
+            error={'password' in errors}
+            helperText={errors.password?.message as string}
+            {...register('password')}
           />
           <TextField
-            id="Name"
             label="Name"
             variant="standard"
             className={'w-100p mt-20'}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              console.log(e.currentTarget.value);
-              setCreateUserFormValues({
-                ...createUserFormValues,
-                name: e.currentTarget.value,
-              });
-            }}
+            error={'name' in errors}
+            helperText={errors.name?.message as string}
+            {...register('name')}
           />
         </Box>
         <div className={'flex justify-end mt-20'}>
-          <Button variant={'contained'} onClick={onCreateButtonClick}>
+          <Button
+            variant={'contained'}
+            type={'submit'}
+            form={'create-user-form'}
+          >
             Submit
           </Button>
         </div>
